0d57ff9ce3897abe80af7f3b1ee6361b9c4c5a1d
杨艺辉11 <122754444@nd.com>
2016-07-17 17:49:27 +0800

    联系 我们

--- gulp/image.js
+++ gulp/image.js
@@ -6,7 +6,15 @@ var browserSync = require('browser-sync');
 var conf = require('./conf');
 
 gulp.task("image", function() {
-    gulp.src(conf.paths.src + "/**/*.{png,jpg,gif,ico}")
+    return gulp.src(conf.paths.src + "/**/*.{png,jpg,gif,jpeg,ico,eot,svg,ttf,woff}")
+        .pipe(cache())
+        .pipe(gulp.dest(conf.paths.tmp))
+        .pipe(browserSync.stream());
+});
+
+
+gulp.task("image-dist", function() {
+    return gulp.src(conf.paths.src + "/**/*.{png,jpg,gif,jpeg,ico,eot,svg,ttf,woff}")
         .pipe(cache(imagemin({
             optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
                         progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
@@ -15,13 +23,6 @@ gulp.task("image", function() {
             svgoPlugins: [{ removeViewBox: false }], //不要移除svg的viewbox属性
                         use: [pngquant()] //使用pngquant深度压缩png图片的imagemin插件
         })))
-        .pipe(gulp.dest(conf.paths.tmp))
-        .pipe(browserSync.stream());
-});
-
-
-gulp.task("image-dist", function() {
-    gulp.src(conf.paths.src + "/**/*.{png,jpg,gif,ico}")
         .pipe(gulp.dest(conf.paths.dist))
         .pipe(browserSync.stream());
 });
